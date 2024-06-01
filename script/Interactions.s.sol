63f2260a7eb33ac07bb98105060ff3033ec44d43
Vivek <vivek.mitra@outlook.com>
2024-06-01 16:22:17 +1000

    tests separated as per type

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,12 +2,27 @@
 
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded with $s", SEND_VALUE);
+    }
+
     function run() external {
         // Deploy FundMe contract - most recently deployed contract - easier
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        fundFundMe(mostRecentlyDeployed);
     }
 }
 
