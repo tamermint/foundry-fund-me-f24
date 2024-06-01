28db4c282c9c2d75939ed2393c9ea0fffa2f2a98
Vivek <vivek.mitra@outlook.com>
2024-06-01 15:53:50 +1000

    foundry devops installed and remappings updated

---
+++ script/Interactions.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    function run() external {
+        // Deploy FundMe contract - most recently deployed contract - easier
+    }
+}
+
+contract WithdrawFundMe is Script {}
